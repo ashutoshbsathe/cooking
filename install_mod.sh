aaa1620fec59f07b966fb7660687f8316c930d57
Ashutosh Sathe <2ashutoshbs@gmail.com>
2024-11-09 17:52:51 +0530

    kitchen setup

--- install_mod.sh
+++ install_mod.sh
@@ -2,12 +2,14 @@
 source .env
 
 MOD_NAME="p2agent"
-MOD_ROOT_DIR="$P2_ROOT_DIR/../../steamapps/sourcemods/$MOD_NAME"
+MOD_ROOT_DIR="$P2_ROOT_DIR/../../sourcemods/$MOD_NAME"
 
 echo $P2_ROOT_DIR
 echo $MOD_ROOT_DIR
 
-mkdir -p $MOD_ROOT_DIR/cfg
-mkdir -p $MOD_ROOT_DIR/maps/soundcache
+mkdir -p "$MOD_ROOT_DIR/cfg"
+mkdir -p "$MOD_ROOT_DIR/maps/soundcache"
+
+tree "$MOD_ROOT_DIR"
 
 cp -r "$MOD_ROOT_DIR/maps/soundcache" "$P2_ROOT_DIR/portal2_dlc2/maps/soundcache"
